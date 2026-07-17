e7a685b469dae9d46e355faf077be5ca301fca91
YasminMomo <yasminmomorios20@gmail.com>
2026-07-17 12:22:21 +0800

    Day 16 - underscore in primitives, references, object lifecycle, and garbage collection

--- Chapter1/NamingConflictLesson.java
+++ Chapter1/NamingConflictLesson.java
@@ -6,5 +6,5 @@ public class NamingConflictLesson {
     Date d1;
     java.sql.Date d2; //this is to avoid naming conflict between java.util.Date and java.sql.Date
     otherFolder.Sunflower s1;
-    otherFolder.Makahiya m1;
+    otherFolder.a.Makahiya m1;
 }
---
+++ Chapter1/ReferenceLesson.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class ReferenceLesson {
+    // Reference types are used to refer to objects
+    public static void main(String[] args) {
+        java.util.Date today = new java.util.Date();
+        String greeting = "How are you?";
+        System.out.println(args[1]); // cli: for args - java ReferenceLesson.java value(depending on index)
+    }
+}
