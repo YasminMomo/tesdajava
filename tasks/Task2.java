38c82f70efdc7e64e0988e5fb7a4b0a44d3406ef
YasminMomo <yasminmomorios20@gmail.com>
2026-07-30 11:28:33 +0800

    Accomplished Task 2

---
+++ tasks/Task2.java
@@ -0,0 +1,15 @@
+package tasks;
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 1;
+        int i = 3110;
+        float f = 2;
+        boolean bool = true;
+        char c = 'H';
+
+        System.out.println(c + "" + i + " w" + b + "r" + s + "d " + f + " " + bool);
+    }
+}
