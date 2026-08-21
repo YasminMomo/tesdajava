31d927f35e5bbc158ebec6d7584c7944e2a70c90
YasminMomo <yasminmomorios20@gmail.com>
2026-08-22 00:01:02 +0800

    Day 30 - Method Structure(Acces Modifier, Optional Specifiers, Return Type, Method Name, Parameter List, Optional Exception List, and Method Body)

--- chapter4/ReturnType.java
+++ chapter4/ReturnType.java
@@ -1,5 +1,7 @@
 package chapter4;
 
+import java.lang.invoke.StringConcatException;
+
 public class ReturnType {
     void startEngine() {
         System.out.println("Engine is running");
@@ -24,6 +26,28 @@ public class ReturnType {
 
     int addTwoNumbers(int a, int b) {
         return a + b;
+    }
+
+    //optional exception
+    int compareTwoStrings(String a, String b) throws StringConcatException{
+        return 0;
+    }
+    int compareThreeStrings(String a, String b,String c) throws StringConcatException, StringIndexOutOfBoundsException{
+        return 0;
+    }
+
+    //method body
+    void emptyBody(){}
+    
+    void withBody(){
+        System.out.println("with method body");
+    }
+    
+    boolean withBodyUsingCondition(int a){
+        boolean result = false;
+        if(a==1)
+            result = true;
+        return result;
     }
 
     public static void main(String[] args) {
